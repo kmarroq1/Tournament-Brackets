62bbce991d412f9ae4327de31dda10ea9bd16079
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-15 20:39:33 -0400

    Reconfigured test settings

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,16 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const { ɵCodegenComponentFactoryResolver } = require('@angular/core');
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-spec-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -27,18 +30,21 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/project2'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: ['spec']
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    autoWatch: ɵCodegenComponentFactoryResolver,
+    browsers: ['FirefoxHeadless'],
+    customLaunchers: {
+      'FirefoxHeadless': {
+        base: 'Firefox',
+        flags: [ '-headless', ],
+      }
+    },
+    singleRun: true,
     restartOnFileChange: true
   });
 };
