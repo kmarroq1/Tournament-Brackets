affd1754b5b844d6256c3a641df13699e5cf308f
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-16 18:15:07 -0400

    Added navigation components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './tournament/hello/hello.component';
+import { RegistrationComponent } from './tournament/registration/registration.component';
+import { BracketsComponent } from './tournament/brackets/brackets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Welcome', component: HelloComponent},
+  { path: 'Registration', component: RegistrationComponent},
+  { path: 'Brackets', component: BracketsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './tournament/hello/hello.component';
+import { RegistrationComponent } from './tournament/registration/registration.component';
+import { BracketsComponent } from './tournament/brackets/brackets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    RegistrationComponent,
+    BracketsComponent
   ],
   imports: [
     BrowserModule,
