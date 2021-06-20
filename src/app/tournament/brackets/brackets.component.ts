6338df1035e2d53b0516bf4dc91ab9393a22f730
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-20 05:16:47 -0400

    Added brackets first match

--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { RosterService } from 'src/app/services/roster.service';
 
 @Component({
   selector: 'app-brackets',
   templateUrl: './brackets.component.html',
-  styleUrls: ['./brackets.component.css']
+  styleUrls: ['./brackets.component.css'],
 })
 export class BracketsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public roster: string[];
+  public currentPlayers: string[];
+  public roundNumber: number;
+  public matches: number[];
+  constructor(private rosterService: RosterService) {
+    this.roster = this.rosterService.getContestants();
+    this.currentPlayers = this.roster;
+    this.roundNumber = 1;
+    this.initializeMatches();
   }
 
+  initializeMatches() {
+    this.matches = [];
+    for (var index = 1; index < (this.currentPlayers.length / 2) + 1; index++)
+      this.matches.push(index);
+  }
+  trackByFn(index: any, item: any) {
+    return index;
+  }
+
+  onSubmit() {
+    this.roundNumber++;
+    this.currentPlayers.pop();
+    this.currentPlayers.pop();
+  }
+  ngOnInit(): void {
+    this.matches.length = this.currentPlayers.length / 2;
+  }
 }
