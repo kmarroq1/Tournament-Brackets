82c6e88c6801dbccba46f77cef22dd050f53c782
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-23 18:54:04 -0400

    Added brackets validation

--- src/app/tournament/brackets/brackets.component.spec.ts
+++ src/app/tournament/brackets/brackets.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RosterService } from 'src/app/services/roster.service';
+import { FormsModule } from '@angular/forms';
 import { BracketsComponent } from './brackets.component';
 
 describe('BracketsComponent', () => {
+  let form: FormsModule;
   let component: BracketsComponent;
   let fixture: ComponentFixture<BracketsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BracketsComponent ]
-    })
-    .compileComponents();
+      imports: [FormsModule],
+      declarations: [BracketsComponent],
+      providers: [RosterService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +22,9 @@ describe('BracketsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should not submit', () => {
+    expect(function () {
+      component.onSubmit();
+    }).toThrowError('Not enough players registered for a match');
   });
 });
--- src/app/tournament/brackets/brackets.component.ts
+++ src/app/tournament/brackets/brackets.component.ts
@@ -11,24 +11,19 @@ export class BracketsComponent implements OnInit {
   public roundWinners: string[];
   public roundNumber: number;
   public matches: number[];
+  public message: string;
+
   constructor(private rosterService: RosterService) {
     this.currentPlayers = this.rosterService.getContestants();
     this.roundNumber = 1;
     this.matches = [];
     this.roundWinners = [];
+    this.message = '';
   }
 
-  addMatches() {
-    for (var index = 1; index < this.currentPlayers.length / 2 + 1; index++)
-      this.matches.push(index);
-  }
-  trackByFn(index: any, item: any) {
-    return index;
-  }
-
-  onSubmit() {
+  setMatches() {
     if (this.currentPlayers.length <= 1) {
-      return;
+      throw new Error('Not enough players registered for a match');
     } else if (this.currentPlayers.length === 2) {
       this.rosterService.resetContestants();
       this.matches.pop();
@@ -40,6 +35,20 @@ export class BracketsComponent implements OnInit {
       this.matches.pop();
       this.roundNumber++;
     }
+  }
+
+  onSubmit() {
+    this.message = '';
+
+    //validate
+    let modifiedWinners = this.roundWinners.filter((winner) => winner !== '');
+    if (modifiedWinners.length != this.matches.length) {
+      this.message = 'Please select a winner for each match';
+      return;
+    }
+
+    this.setMatches();
+
     this.currentPlayers.splice(0, this.currentPlayers.length);
     for (let index = 0; index < this.roundWinners.length; index++) {
       this.currentPlayers[index] = this.roundWinners[index];
@@ -51,6 +60,16 @@ export class BracketsComponent implements OnInit {
       this.roundWinners = [];
     }
   }
+
+  addMatches() {
+    for (var index = 1; index < this.currentPlayers.length / 2 + 1; index++)
+      this.matches.push(index);
+  }
+
+  trackByFn(index: any, item: any) {
+    return index;
+  }
+
   ngOnInit(): void {
     this.addMatches();
   }
